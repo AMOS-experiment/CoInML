c3858e86525b974d58ff1af228c6476e701c7dd5
Hazem Daoud <56599590+daoudhaz@users.noreply.github.com>
2025-12-05 13:49:00 -0800

    Moving prefect worker outside Docker

---
+++ start_worker.sh
@@ -0,0 +1,10 @@
+source .env
+
+export PREFECT_WORK_DIR=$PREFECT_WORK_DIR
+prefect config set PREFECT_API_URL=$PREFECT_API_URL
+
+prefect work-pool create sculpt-pool --type "process"
+prefect work-pool set-concurrency-limit sculpt-pool $PREFECT_WORK_POOL_CONCURRENCY
+prefect deploy --all
+
+prefect worker start --pool sculpt-pool --limit $PREFECT_WORKER_LIMIT --with-healthcheck
